cc43ed06467172e6a3d96c3b85db9ef590ac6c4e
jon <jon@jons-MacBook-Pro.local>
2019-05-29 21:09:05 -0400

    latest

--- src/containers/appContainer.js
+++ src/containers/appContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { logout } from 'actions'
 import { TopBar } from 'components'
+import BootstrapTable from 'react-bootstrap-table2'
 
 class _AppContainer extends Component {
   constructor(props) {
@@ -10,9 +11,18 @@ class _AppContainer extends Component {
 
 
   render() {
+    const products = [{
+      productId:'test',
+      name:'test',
+      price:30
+    }]
     return (
       <div className='app-container'> 
-        <TopBar user={{name:'hello'}} />
+         <BootstrapTable data={ products } striped hover condensed>
+            <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+            <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+            <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+        </BootstrapTable>
       </div>
     )
   }
